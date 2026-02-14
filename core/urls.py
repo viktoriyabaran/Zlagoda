1be3b4f80da778395f0ceb9cb9a32adefd90ad6c
viktoriyabaran <viktoriabaran83@gmail.com>
2026-02-14 16:19:04 +0200

    WA: Add user login view and template

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import LoginView, home
 
 app_name = "core"
 
 urlpatterns = [
     path("home/", home, name="home"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+
+from core.services import AuthService, IAuthService
+
+from .forms import UserForm
 
 
-# Define core views (login, logout, dashboard) here
+class LoginView(View):
+    auth_service: IAuthService = AuthService()
+
+    def get(self, request):
+        form = UserForm()
+        return render(request, "login.html", {"form": form})
+
+    def post(self, request):
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = self.auth_service.authenticate(
+                form.cleaned_data["username"],
+                form.cleaned_data["password"],
+            )
+            if user:
+                request.session["user_id"] = user["id"]
+                return redirect("core:home")
+            form.add_error(None, "Invalid credentials")
+        return render(request, "login.html", {"form": form})
+
+
 def home(request):
     return render(request, "home.html")
