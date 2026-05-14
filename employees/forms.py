6a7e787a630c469ce583e479c917eb48748fbdfe
iraholdynska <i.holdynska346@gmail.com>
2026-05-14 00:59:07 +0000

    SL: Add employee create form

--- employees/forms.py
+++ employees/forms.py
@@ -1,7 +1,29 @@
-from .validators import validate_age
+from django import forms
+
+from .models import EmployeeRole
+from .validators import validate_age, validate_salary
+
 
 class EmployeeForm(forms.ModelForm):
+    id_employee = forms.CharField(max_length=10)
+    empl_surname = forms.CharField(max_length=50)
+    empl_name = forms.CharField(max_length=50)
+    empl_patronymic = forms.CharField(max_length=50, required=False)
+    empl_role = forms.ChoiceField(choices=EmployeeRole.choices)
+    salary = forms.DecimalField(max_digits=13, decimal_places=4, min_value=0)
+    date_of_birth = forms.DateField()
+    date_of_start = forms.DateField()
+    phone_number = forms.CharField(max_length=13)
+    city = forms.CharField(max_length=50)
+    street = forms.CharField(max_length=50)
+    zip_code = forms.CharField(max_length=9)
+
     def clean_date_of_birth(self):
-        value = self.cleaned_data['date_of_birth']
+        value = self.cleaned_data["date_of_birth"]
         validate_age(value)
+        return value
+
+    def clean_salary(self):
+        value = self.cleaned_data["salary"]
+        validate_salary(value)
         return value
--- employees/validators.py
+++ employees/validators.py
@@ -1,8 +1,15 @@
 from datetime import date
+
 from django.core.exceptions import ValidationError
 
+
 def validate_age(value):
-    now = data.now()
+    now = date.today()
     age = now.year - value.year - ((now.month, now.day) < (value.month, value.day))
     if age < 18:
         raise ValidationError("User must be at least 18 years old.")
+
+
+def validate_salary(value):
+    if value < 0:
+        raise ValidationError("Salary must be a positive number")
