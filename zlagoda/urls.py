bc3b7be1312b26e4af97ba2166ecdd93d9862358
viktoriyabaran <viktoriabaran83@gmail.com>
2026-05-15 12:33:26 +0300

    WA: Redirect to home page on empty url

--- zlagoda/urls.py
+++ zlagoda/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('__reload__/', include('django_browser_reload.urls')),
-    path('', include('core.urls')),
-    path('employees/', include('employees.urls')),
-    path('products/', include('products.urls')),
-    path('sales/', include('sales.urls')),
-    path('customers/', include('customers.urls')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
+    path("", RedirectView.as_view(pattern_name="core:home", permanent=False)),
+    path("", include("core.urls")),
+    path("employees/", include("employees.urls")),
+    path("products/", include("products.urls")),
+    path("sales/", include("sales.urls")),
+    path("customers/", include("customers.urls")),
 ]
