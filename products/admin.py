4116980c04fd94f3abef597f9640d486a9ea125b
DarynaHalytska <smartsmj320@gmail.com>
2026-05-15 18:01:32 +0000

    DB: Remove category_number

--- products/admin.py
+++ products/admin.py
@@ -6,7 +6,7 @@ from .models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = [
-        "category_number",
+        "id",
         "category_name",
     ]
     search_fields = [
--- products/models.py
+++ products/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 # Define Category, Product, StoreProduct models here
 class Category(models.Model):
-    category_number = models.IntegerField(primary_key=True)
     category_name = models.CharField(max_length=50)
 
 
