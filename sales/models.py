b8043ae224945052ef22d3a170a836a2944eb0f3
viktoriyabaran <viktoriabaran83@gmail.com>
2026-05-16 16:56:48 +0300

    DB: Add Sale table

--- sales/models.py
+++ sales/models.py
@@ -29,3 +29,22 @@ class Check(models.Model):
 
     def __str__(self):
         return f"Check {self.check_number}: {self.sum_total} by {self.id_employee} on {self.print_date}"
+
+
+class Sale(models.Model):
+    pk = models.CompositePrimaryKey("UPC", "check_number")
+    UPC = models.ForeignKey(
+        "products.StoreProduct",
+        on_delete=models.DO_NOTHING,
+        db_column="UPC",
+    )
+    check_number = models.ForeignKey(
+        Check,
+        on_delete=models.CASCADE,
+        db_column="check_number",
+    )
+    product_number = models.IntegerField()
+    selling_price = models.DecimalField(max_digits=13, decimal_places=4)
+
+    def __str__(self):
+        return f"Sale {self.UPC}, check {self.check_number}: {self.product_number} for {self.selling_price}"
