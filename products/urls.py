8f82a53cca9a580cd0da71cff6e14b624042aea5
DarynaHalytska <smartsmj320@gmail.com>
2026-05-16 19:38:49 +0000

    DB: Add Category urls

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import AddCategoryView, categories
 
 app_name = 'products'
 
-urlpatterns = []
+urlpatterns = [
+    path("categories/", categories, name="categories"),
+    path("categories/add/", AddCategoryView.as_view(), name="add-category"),
+]
