54c4d552eaf23d20eb33ee5d4efb83e7f837ca9b
DarynaHalytska <smartsmj320@gmail.com>
2026-05-16 19:20:54 +0000

    DB: Change repository

--- products/repository.py
+++ products/repository.py
@@ -1,13 +1,13 @@
-from core.db import execute_single, execute_all, execute_update
+from core.db import execute_single, execute_query, execute_write
 
-def get_category_by_id(user_id: int):
+def get_category_by_id(category_id: int):
     return execute_single("SELECT * FROM products_category WHERE id = %s", [category_id])
 
 def get_all_categories():
-    return execute_all("SELECT * FROM products_category")
+    return execute_query("SELECT * FROM products_category")
 
 def create_category(category_name: str):
-    execute_update(
+    execute_write(
         "INSERT INTO products_category (category_name) VALUES (%s)",
         [category_name],
     )
