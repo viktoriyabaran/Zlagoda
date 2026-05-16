4e907eb58f0c744de13ceae1c4a98acf45232513
DarynaHalytska <smartsmj320@gmail.com>
2026-05-16 19:31:21 +0000

    DB: Add Category views

--- products/views.py
+++ products/views.py
@@ -1,1 +1,27 @@
 # Define product views here
+from django.shortcuts import redirect, render
+from django.views import View
+
+from core.decorators import login_required
+from .forms import CategoryForm
+from .services import CategoryService
+
+class AddCategoryView(View):
+    category_service = CategoryService()
+
+    def get(self, request):
+        form = CategoryForm()
+        return render(request, "products/add-category.html", {"form": form})
+
+    def post(self, request):
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            self.category_service.create(form.cleaned_data["category_name"])
+            return redirect("products:categories")
+        return render(request, "products/add-category.html", {"form": form})
+
+@login_required
+def categories(request):
+    service = CategoryService()
+    all_categories = service.get_all()
+    return render(request, "products/categories.html", {"categories": all_categories})
