6e5165b828b181705954ba01ca5902b68cb71478
DarynaHalytska <smartsmj320@gmail.com>
2026-05-17 17:43:27 +0000

    WA: Add customers urls

--- customers/urls.py
+++ customers/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import AddCustomerView, GetCustomersView
 
 app_name = 'customers'
 
-urlpatterns = []
+urlpatterns = [
+    path("customers/", GetCustomersView.as_view(), name="customers"),
+    path("customers/add/", AddCustomerView.as_view(), name="add-customer"),
+]
