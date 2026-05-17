1a8efc24c368baf3ad4d2c31298e5fdb6d217b7c
DarynaHalytska <smartsmj320@gmail.com>
2026-05-17 18:18:23 +0000

    WA: Fix customers urls paths

--- customers/urls.py
+++ customers/urls.py
@@ -4,6 +4,6 @@ from .views import AddCustomerView, GetCustomersView
 app_name = 'customers'
 
 urlpatterns = [
-    path("customers/", GetCustomersView.as_view(), name="customers"),
-    path("customers/add/", AddCustomerView.as_view(), name="add-customer"),
+    path("", GetCustomersView.as_view(), name="customers"),
+    path("add/", AddCustomerView.as_view(), name="add-customer"),
 ]
