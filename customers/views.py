c2fa8f014beed6d825904a3f057ce3d09de7323d
DarynaHalytska <smartsmj320@gmail.com>
2026-05-17 11:19:55 +0000

    WA: Add Customers views

--- customers/views.py
+++ customers/views.py
@@ -1,1 +1,27 @@
-# Define customer views here
+from django.shortcuts import redirect, render
+from django.views import View
+
+from core.decorators import login_required
+from .forms import CustomerCardForm
+from .services import CustomerService
+
+class AddCustomerView(View):
+    customer_service = CustomerService()
+
+    def get(self, request):
+        form = CustomerCardForm()
+        return render(request, "customers/add-customer.html", {"form": form})
+
+    def post(self, request):
+        form = CustomerCardForm(request.POST)
+        if form.is_valid():
+            self.customer_service.create(form.cleaned_data)
+            return redirect("customers:customers")
+        return render(request, "customers/add-customer.html", {"form": form})
+
+
+@login_required
+def customers(request):
+    service = CustomerService()
+    all_customers = service.get_all()
+    return render(request, "customers/customers.html", {"customers": all_customers})
