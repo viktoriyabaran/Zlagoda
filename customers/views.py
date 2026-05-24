eb7c699c43eae818dfb087da69894db3c5bfda42
viktoriyabaran <viktoriabaran83@gmail.com>
2026-05-24 16:09:59 +0300

    SL: Display customer card number

--- customers/views.py
+++ customers/views.py
@@ -9,10 +9,11 @@ from .forms import CustomerCardForm
 from .services import CustomerService, ICustomerService
 
 CUSTOMER_COLUMNS = [
+    {"key": "id", "label": "Card Number", "sortable": False},
     {"key": "cust_surname", "label": "Surname", "sortable": True},
     {"key": "cust_name", "label": "Name", "sortable": False},
     {"key": "phone_number", "label": "Phone", "sortable": False},
-    {"key": "percent", "label": "Discount %", "sortable": True},
+    {"key": "percent", "label": "Discount %", "sortable": False},
 ]
 CUSTOMER_SORTABLE = {c["key"] for c in CUSTOMER_COLUMNS if c["sortable"]}
 
